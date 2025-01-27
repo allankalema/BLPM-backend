058d46cbc3cef8d1999c420fdc995ddfd7823e5c
allankalema4 <allankalema4@gmail.com>
2025-01-27 17:25:28 +0300

    added the transfer  models

--- transfer/admin.py
+++ transfer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Transfer
 
-# Register your models here.
+class TransferAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user', 'land_title', 'status', 'transfer_date')
+    list_filter = ('status', 'transfer_date')
+    search_fields = ('from_user__username', 'to_user__username', 'land_title__land_title')
+    ordering = ('-transfer_date',)
+
+admin.site.register(Transfer, TransferAdmin)
--- transfer/models.py
+++ transfer/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings  # Import the settings to use AUTH_USER_MODEL
+from property.models import Property  # Importing Property model
 
-# Create your models here.
+class Transfer(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('successful', 'Successful'),
+        ('rejected', 'Rejected'),
+    ]
+    
+    from_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        related_name='transfers_from', 
+        on_delete=models.CASCADE
+    )
+    to_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        related_name='transfers_to', 
+        on_delete=models.CASCADE
+    )
+    land_title = models.ForeignKey(Property, related_name='transfers', on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='pending',
+    )
+    transfer_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Transfer of {self.land_title.land_title} from {self.from_user.username} to {self.to_user.username} - {self.status}"
+
+    class Meta:
+        verbose_name = "Land Transfer"
+        verbose_name_plural = "Land Transfers"
+        ordering = ['-transfer_date']
