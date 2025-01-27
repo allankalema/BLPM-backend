1cb9f346b8dd2b6946444991943a98ab653f3d20
allankalema4 <allankalema4@gmail.com>
2025-01-27 12:33:05 +0300

    location table added

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    village = models.CharField(max_length=100)
+    parish = models.CharField(max_length=100)
+    subcounty = models.CharField(max_length=100)
+    county = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, default='Uganda')
+
+    def __str__(self):
+        return f"{self.village}, {self.parish}, {self.subcounty}, {self.county}, {self.district}, {self.country}"
+
+    class Meta:
+        verbose_name_plural = "Locations"
